e8be77ef26734a33918ad296d6eb1e3dab31746c
humenspider7 <iamyusuf6@aol.com>
2017-01-27 19:18:01 -0500

    Added Random Codes

---
+++ CS_Chapter1/Random Codes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Codes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <=1000; i = i + 1)
+            {
+                if (i % 2 = 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
